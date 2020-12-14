117f88c7229d1eea40009a57b4e4fecdef5b1a82
kwat <jorgeglez1616@gmail.com>
2020-12-13 23:59:42 -0600

    removed test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('test_app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/WIP/i);
-  expect(linkElement).toBeInTheDocument();
-});
